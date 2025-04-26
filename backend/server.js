30b1cdbb4411a224a2332ddcc306d8bcc5779f01
vsvikashrm6 <vsvikasshrm6@gmail.com>
2025-04-26 14:35:02 +0530

    Backend Routing

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js"
-<<<<<<< HEAD
 import productRouter from "./router/productRouter.js";
 
 dotenv.config();
@@ -9,15 +8,5 @@ const app = express();
 connectDb();
 app.use(express.json());
 app.use("", productRouter)
-=======
-
-dotenv.config();
-const app = express();
-
-app.get('/', (req, res)=>{
-    connectDb();
-    res.send('Hello worldfdsgfd');
-})
->>>>>>> faacb99c453b79c6931c5d2d15f939f9b9af6ca8
 
 app.listen(3000);
