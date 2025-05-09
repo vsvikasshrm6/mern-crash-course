d7914509d16f04e8ee789eebbc89e54feb64fd13
Sharma <vsvikasshrm6@gmail.com>
2025-05-09 13:04:15 +0530

    ZustandComplete

--- frontend/src/store/products.js
+++ frontend/src/store/products.js
@@ -1,28 +1,62 @@
 import { create } from 'zustand';
 export const useProductStore = create((set) => ({
-    product: [],
-    setProduct: (product) => set({product}),
-    createProduct: async (newProduct) => {
+    products: [],
+    setProducts: (products) => set({products}),
+    createProducts: async (newProduct) => {
         if (!newProduct.name || !newProduct.price || !newProduct.image) {
             return { success: 'false', message: 'Product not created' }
         }
         const res = await fetch('http://localhost:5000/', {
             method: "Post",
+            headers :{
+                "Content-Type" : "application/json"
+            },
             body: JSON.stringify(newProduct),
         })
-        if (res.success === 'false') {
+        const data = res.json();
+        if (data.success === 'false') {
             return { success: 'false', message: 'Product not created' }
         }
-
-        set((state) => ({product : [...state.products, res.data.data]}));
+        set((state) => ({products : [...state.products, data.data]}));
         return { success: 'true', message: 'Product created successfully' }
     },
     fetchProduct : async ()=>{
         const res = await fetch('api/allProducts', {
             method :"Get"
         })
-        const fetchedProduct = res.json();
-        set(fetchedProduct.data);
+        const data = res.json();
+        if (data.success === 'false') {
+            return { success: 'false', message: 'Product not fetched' }
+        }
+        set({products : data.data})
         return { success: 'true', message: 'Product retrieved successfully' }
+    },
+    deleteProduct : async (pid)=>{
+      const res = await fetch(`http:/localhost:5000/deleteProduct/${pid}`,
+        {method : "DELETE"}
+      )
+      const data = res.json();
+      if (data.success === 'false') {
+        return { success: 'false', message: 'Product not deleted' }
+    }
+    set((state)=>({products : state.products.filter((product)=>product.id !==pid)}));
+    return { success: 'true', message: 'Product deleted successfully' }
+    },
+    updateProduct : async (updatedProduct)=>{
+        const res = await fetch("http:localhost:5000/updateProduct",
+            {
+                method : "PUT",
+                headers :{
+                    "Content-Type": "application/json"
+                },
+                body : JSON.stringify(updatedProduct),
+            }
+        )
+        const data = res.json();
+        if (data.success === 'false') {
+            return { success: 'false', message: 'Product not updated' }
+        }
+        set((state)=>({products : state.products.map((product)=>{return product.id !=updatedProduct.id ?product: updatedProduct})}));
+        return { success: 'true', message: 'Product updated successfully' }
     } 
 }))
